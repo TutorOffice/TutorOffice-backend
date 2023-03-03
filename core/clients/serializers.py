999915e92bfceb9f7039d677036fd2f25e13128b
SanUlrich <SanUlrich@mail.ru>
2023-03-03 17:43:11 +0100

    clients: add serializers.py

---
+++ core/clients/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from .models import User, Teacher, Student
+from django.db.transaction import atomic
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+
+# Сериализатор для обработки формы регистрации
+class RegisterSerializer(serializers.ModelSerializer):
+    is_teacher = serializers.BooleanField(
+        required=True,
+        write_only=True,
+    )
+    password = serializers.CharField(
+        required=True,
+        write_only=True,
+        validators=[
+            validate_password,
+        ]
+    )
+    password2 = serializers.CharField(
+        required=True,
+        write_only=True,
+    )
+
+# Поля взяты из .models.User
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'patronymic_name',
+            'last_name',
+            'email',
+            'phone',
+            'is_teacher',
+            'password',
+            'password2',
+        ]
+
+# Проверка совпадающих паролей в форме регистрации
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise ValidationError({
+                'password': 'Пароли не совпадают',
+                'password2': 'Пароли не совпадают',
+            })
+        attrs.pop('password2')
+        return attrs
+
+# Переопределение метода create.
+# В случае ошибочного ввода данных НЕ сохраняет строки в БД
+    @atomic
+    def create(self, validated_data):
+        role = validated_data.pop('is_teacher')
+        password = validated_data.pop('password')
+        print(validated_data)
+        user = super().create(validated_data)
+        user.password = make_password(password)
+        user.save()
+        if role:
+            Teacher.objects.create(user=user)
+        else:
+            Student.objects.create(user=user)
+        return user
