a10983d10857b35e909538e8d4f4fd792b615fd0
SanUlrich <SanUlrich@mail.ru>
2023-03-03 17:44:03 +0100

    clients: add RegisterView

--- core/clients/views.py
+++ core/clients/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from clients.models import User
+from clients.serializers import RegisterSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.viewsets import GenericViewSet
 # Create your views here.
+
+
+class RegisterView(CreateAPIView, GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
