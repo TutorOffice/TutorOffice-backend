bf5ce7e93cd00c1d1349743250de00fc259776d7
Финатова Ирина <irinafinatova@yandex.ru>
2023-03-29 18:22:33 +0300

    миграции

--- core/lessons/serializers.py
+++ core/lessons/serializers.py
@@ -1,20 +1,40 @@
+from datetime import date
 from .models import Lesson, Homework
-from clients.models import TeacherStudent, Teacher
+from clients.models import Teacher
 from django.shortcuts import get_object_or_404
 
 from rest_framework.serializers import (
     ModelSerializer,
     SlugRelatedField,
     CurrentUserDefault,
+    ValidationError
 )
 
-class HomeworkSerializer(ModelSerializer):
+
+class HomeworkTeacherSerializer(ModelSerializer):
+    """Сериализатор для представления ДЗ для учителя"""
+    teacher = SlugRelatedField(slug_field='pk',
+                               default=CurrentUserDefault(),
+                               read_only=True)
+
     class Meta:
         model = Homework
-        fields = ('title', 'text', 'comment')
+        fields = ('teacher', 'title', 'text', 'comment')
+
+
+class HomeworkStudentSerializer(ModelSerializer):
+    """Сериализатор для представления ДЗ для студента"""
+    teacher = SlugRelatedField(slug_field='pk',
+                               read_only=True)
+
+    class Meta:
+        model = Homework
+        fields = ('teacher', 'title', 'text', 'comment')
 
 
 class SubjectSlugRelated(SlugRelatedField):
+    """Возможность при создании урока выбора
+     предмета урока только из предметов учителя"""
     def get_queryset(self):
         request = self.context.get('request', None)
         teacher = get_object_or_404(Teacher, user=request.user)
@@ -22,13 +42,17 @@ class SubjectSlugRelated(SlugRelatedField):
 
 
 class TeacherStudentSlugRelated(SlugRelatedField):
+    """Возможность при создании урока выбора
+    студента только из студентов учителя"""
     def get_queryset(self):
         request = self.context.get('request', None)
         teacher = get_object_or_404(Teacher, user=request.user)
-        return TeacherStudent.objects.filter(teacher=teacher)
+        return teacher.teacherstudents.all()
 
 
 class HomeworkSlugRelated(SlugRelatedField):
+    """Возможность при создании урока
+     выбора ДЗ только из ДЗ учителя"""
     def get_queryset(self):
         request = self.context.get('request', None)
         teacher = get_object_or_404(Teacher, user=request.user)
@@ -36,6 +60,21 @@ class HomeworkSlugRelated(SlugRelatedField):
 
 
 class LessonTeacherSerializer(ModelSerializer):
+    """Сериализатор для представления уроков для учителя"""
+    def validate(self, data):
+        """
+        Валидация даты и времени урока
+        (проверка время окончание урока позже времени начала урока,
+         дата урока не раньше сегодня)
+        """
+        if data['start_time'] > data['end_time']:
+            raise ValidationError(
+                'Время окончание урока должно быть позже времени начала урока!')
+        if data['date'] < date.today():
+            raise ValidationError(
+                'Урок не может быть раньше сегодняшней даты!')
+        return data
+
     subject = SubjectSlugRelated(slug_field='title')
     teacher = SlugRelatedField(slug_field='pk',
                                default=CurrentUserDefault(),
@@ -60,11 +99,12 @@ class LessonTeacherSerializer(ModelSerializer):
 
 
 class LessonStudentSerializer(ModelSerializer):
+    """Сериализатор для представления уроков для студента"""
     subject = SlugRelatedField(slug_field='title', read_only=True)
     teacher = SlugRelatedField(slug_field='pk',
                                read_only=True)
     teacher_student = SlugRelatedField(slug_field='last_name', read_only=True)
-    homework = HomeworkSerializer()
+    homework = HomeworkStudentSerializer(read_only=True)
 
     class Meta:
         model = Lesson
