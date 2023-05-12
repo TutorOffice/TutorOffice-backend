ef4b12ab343539692d0b7b9e9cf486807b693504
Takeshi <orwellj@mail.ru>
2023-05-12 18:23:24 +0300

    add filters

--- core/materials/filters.py
+++ core/materials/filters.py
@@ -1,4 +1,4 @@
-from django_filters import CharFilter, ChoiceFilter, FilterSet
+from django_filters import CharFilter, ChoiceFilter, FilterSet, DateFromToRangeFilter
 
 from .models import TYPECHOICE, Material
 
@@ -6,18 +6,31 @@ from .models import TYPECHOICE, Material
 class MaterialFilter(FilterSet):
     """Фильтры по всем полям модели Lesson"""
 
-    teacher_student = CharFilter(field_name='teacher_student__last_name',
-                                 lookup_expr='iexact',
-                                 label='Фамилия студента')
-    subject = CharFilter(field_name='subject__title',
-                         lookup_expr='iexact',
-                         label='Предмет')
-    text = CharFilter(field_name='text',
-                      lookup_expr='icontains',
-                      label='Встречается в тексте')
-    type = ChoiceFilter(choices=TYPECHOICE,
-                        label='Тип материала')
+    student = CharFilter(
+        field_name='teacher_student__id',
+        label='Ученик',
+    )
+    teacher = CharFilter(
+        field_name='teacher__user__id',
+        label='Репетитор',
+    )
+    subject = CharFilter(
+        field_name='subject__id',
+        label='Предмет',
+    )
+    text = CharFilter(
+        field_name='text',
+        lookup_expr='icontains',
+        label='Встречается в тексте',
+    )
+    type = ChoiceFilter(
+        choices=TYPECHOICE,
+        label='Тип материала',)
+    date = DateFromToRangeFilter(
+        field_name='date',
+        label='Дата',
+    )
 
     class Meta:
         model = Material
-        fields = ('teacher_student', 'subject', 'text', 'type')
+        fields = ('student', 'subject', 'text', 'type')
