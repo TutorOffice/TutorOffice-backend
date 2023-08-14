e11e004667535ca6daa171aa71bf2cd6a26553f2
Takeshi <orwellj@mail.ru>
2023-08-14 20:57:08 +0300

    add docs in prod

--- core/core/urls.py
+++ core/core/urls.py
@@ -14,28 +14,7 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    from drf_yasg import openapi
-    from drf_yasg.views import get_schema_view
-    from rest_framework import permissions
 
-    schema_view = get_schema_view(
-        openapi.Info(
-            title="TutorOffice API",
-            default_version="v1",
-        ),
-        public=True,
-        permission_classes=[permissions.AllowAny],
-    )
     urlpatterns += [
-        path(
-            "swagger/",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="swagger",
-        ),
-        path(
-            "redoc/",
-            schema_view.with_ui("redoc", cache_timeout=0),
-            name="redoc",
-        ),
         path("__debug__/", include("debug_toolbar.urls")),
     ]
